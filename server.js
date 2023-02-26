0d1af29b6d4b9f698ac607583cebac466579f02a
Asad Ansari <115532433+asadansari8840@users.noreply.github.com>
2023-02-26 21:30:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,15 +13,14 @@ process.on("uncaughtException", (err) => {
 
 //config 
 dotenv.config();
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 //Connecting to database
 connectDatabase();
 
